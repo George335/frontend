f2d51cfb62ade03a8e0b218f3674a1edc01103e1
Your Name <you@example.com>
2018-10-15 07:42:40 -0500

    Usuario list creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { UserComponent } from 'src/app/user/user.component';
+
 import { AppComponent } from 'src/app/app.component';
 
 const routes: Routes =[
     { path :'', redirectTo:'/appComponent', pathMatch: 'full' },
-    { path: 'appComponent', component: AppComponent }
+    { path: 'appComponent', component: AppComponent },
+    { path: 'userComponent', component: UserComponent }
 ];
 
 @NgModule({
